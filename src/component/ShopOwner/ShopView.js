f2b6d2f96e0ec3bc1dccadb826341a9a224c35d7
gautam <gautamk.inexture@gmail.com>
2023-06-22 14:34:38 +0530

    change design part of shop view and pizza view

--- src/component/ShopOwner/ShopView.js
+++ src/component/ShopOwner/ShopView.js
@@ -1,9 +1,7 @@
-import React, { Fragment } from 'react';
+import React, {Fragment} from 'react';
 import axios from "axios";
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-
-
+import {useEffect, useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 
 const ShopView = () => {
@@ -22,37 +20,39 @@ const ShopView = () => {
     }, [])
 
     return <div>
-        <table className="table table-bordered">
-            <thead className='table-dark'>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Shop Owner</th>
-                    <th scope="col">Shop Name</th>
-                    <th scope="col">Location</th>
-                    <th></th>
-                </tr>
-            </thead>
-            {shopData.map((datashop, index) => {
-                return (
-                    <Fragment key={index}>
+        <div className="row" style={{ display: 'flex', justifyContent: 'center' }}>
+        {shopData.map((datashop, index) => {
+            return (
+                <Fragment key={index}>
 
-                        <tbody style={{ backgroundImage: "linear-gradient(to right, rgba(255,0,0,0), rgba(255,0,0,1))" }}>
-                            <tr>
-                                <th scope="row">{index + 1}</th>
-                                <td>{datashop.owner}</td>
-                                <td>{datashop.name}</td>
-                                <td>{datashop.location}</td>
-                                {token ?
-                                    <td><button onClick={e => { console.log("view"); navigate(`/user/shop-list-user/pizza-items/${datashop.id}`) }}>View</button></td> :
-                                    <td><button onClick={e => { console.log("view"); navigate(`/shop-list/${datashop.id}/pizza/`) }}>View</button></td>
-                                }
-                            </tr>
-                        </tbody>
-                    </Fragment>
-                )
-            })}
+                        <div className="col-md-3">
+                            <div className="card ml-1 mx-1 mb-2 mt-2 text-md-center" style={{width: "25rem" , background:"aliceblue"}}>
+                                <div className="card-body">
+                                    <h5 className="card-title">Shop Name: {datashop.name}</h5>
+                                    <h6 className="card-subtitle mb-2 text-muted">Owner: {datashop.owner}</h6>
+                                    <p className="card-text">Location: {datashop.location}</p>
+                                    {token ? <>
+                                        <button onClick={e => {
+                                            console.log("view");
+                                            navigate(`/user/shop-list-user/pizza-items/${datashop.id}`)
+                                        }}>View Pizza's
+                                        </button>
+                                    </> : <>
+                                        <button onClick={e => {
+                                            console.log("view");
+                                            navigate(`/shop-list/${datashop.id}/pizza/`)
+                                        }}>View Pizza's
+                                        </button>
+                                    </>}
+                                </div>
+                            </div>
+                    </div>
+                </Fragment>
 
-        </table>
+            )
+        })}
+        </div>
+
     </div>;
 }
 
--- src/component/userFunctionality/PizzaItemView.js
+++ src/component/userFunctionality/PizzaItemView.js
@@ -1,22 +1,23 @@
-import React, { useEffect,useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { useParams, useNavigate } from 'react-router-dom'
-import { pizza_items, items_add_cart } from '../Authentication/Slice/TokenSlice'
-import { Fragment } from 'react'
+import React, {useEffect, useState} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import {useParams, useNavigate} from 'react-router-dom'
+import {pizza_items, items_add_cart} from '../Authentication/Slice/TokenSlice'
+import {Fragment} from 'react'
 import swal from 'sweetalert';
-import { Navbar } from 'react-bootstrap'
+import {Navbar} from 'react-bootstrap'
 
 const PizzaItemView = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    let { id } = useParams();
+    let {id} = useParams();
     const pizza_data = useSelector(state => state.tokenData.pizza_data.data)
-    
-    
+
+
     useEffect(() => {
-        dispatch(pizza_items({ id: id }));
+        dispatch(pizza_items({id: id}));
         // eslint-disable-next-line
     }, [])
+
     function JSalert() {
 
         swal("Yup!", "Pizza Added to Cart!", "success");
@@ -25,13 +26,13 @@ const PizzaItemView = () => {
 
     function JSalertWarn() {
 
-        swal("Uuf!","You have already added pizza from another SHOP!","warning");
+        swal("Uuf!", "You have already added pizza from another SHOP!", "warning");
 
     }
 
     function handleOnClick(e, id) {
         e.preventDefault();
-        dispatch(items_add_cart({ pizza: id, quantity: 1 }))
+        dispatch(items_add_cart({pizza: id, quantity: 1}))
             .then(res => {
                 console.log(res.meta.requestId);
                 if (res.type === "TokenSlice/pizzaCartItems/fulfilled") {
@@ -47,51 +48,38 @@ const PizzaItemView = () => {
             })
     }
 
-    console.log(pizza_data);
-    // console.log(pizza_data.length, "l");
     return (
         <div className='container-fluid'>
-            <div className='row'>
-                <div className='col'>
-                    {pizza_data ?
-
-                        <table className="table table-hover">
-                            <thead className='table-dark'>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Pizza Name</th>
-                                    <th scope="col">Pizza Price</th>
-                                    <th scope="col">Size</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            {pizza_data.map((datashop, index) => {
-                                return (
-                                    <Fragment key={index}>
-
-                                        <tbody>
-                                            <tr>
-                                                <th scope="row">{index + 1}</th>
-                                                <td>{datashop.name}</td>
-                                                <td>{datashop.price}</td>
-                                                <td>{datashop.size}</td>
-                                                <td><button onClick={e => { console.log("clicked"); handleOnClick(e, datashop.id); }}>Add to Cart</button></td>
-                                            </tr>
-                                        </tbody>
-                                    </Fragment>
-                                )
-                            })}
-
-                        </table> : <>No Pizza Available For This Shop Right Now.......</>
-
-
-                    }
-                    <button className='btn-primary' onClick={e => navigate("/user/shop-list-user")}>Back</button>
-                </div>
-
+            <div className="row " style={{display: 'flex', justifyContent: 'center'}}>
+                {pizza_data ? <>
+                    {pizza_data.map((datashop, index) => {
+                        return (
+                            <Fragment key={index}>
+                                <div className="col-md-3">
+                                    <div className="card ml-1 mx-1 mb-2 mt-2 text-md-center"
+                                         style={{width: "25rem", background: "aliceblue"}}>
+                                        <div className="card-body">
+                                            <h5 className="card-title">Pizza Name: {datashop.name}</h5>
+                                            <h6 className="card-subtitle mb-2 text-muted">Price: {datashop.price}</h6>
+                                            <p className="card-text">Size: {datashop.size}</p>
+                                            <button onClick={e => {
+                                                console.log("clicked");
+                                                handleOnClick(e, datashop.id);
+                                            }}>Add to Cart
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Fragment>
+                        )
+                    })}
+                </> : <>No Pizza Available For This Shop Right Now.......</>}
             </div>
+            <button className='btn-primary' onClick={e => navigate("/user/shop-list-user")}>Back</button>
 
         </div>
+
+
     )
 }
 
